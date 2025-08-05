b467f82a083ddea37867b646794759eec07ec53b
Demonkinng <angelchuncho5@gmail.com>
2025-08-05 12:53:00 -0500

    Add initial database schema and diagrams for movie database
    - Created JSON schema for the staging movie database (stg_movies-db) including tables such as city, staff, rental, film_category, customer, and more.
    - Added PNG diagrams representing the data warehouse and staging movie database structures.

--- 3.estructuras_dwh_movies.sql
+++ 3.estructuras_dwh_movies.sql
@@ -1,20 +1,3 @@
-CREATE TABLE dim_actor (
-    actor_id integer PRIMARY KEY,
-    film_id integer,
-    full_name character varying,
-    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-    FOREIGN KEY (film_id) REFERENCES Dim_film(film_id)
-);
-
-CREATE TABLE dim_customer (
-    customer_id integer PRIMARY KEY,
-    full_name character varying,
-    active boolean,
-    geography_id integer,
-    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-    FOREIGN KEY (geography_id) REFERENCES Dim_geography(geography_id)
-);
-
 CREATE TABLE dim_film (
     film_id integer PRIMARY KEY,
     title character varying(255),
@@ -27,20 +10,47 @@ CREATE TABLE dim_film (
     fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE dim_actor (
+    actor_id integer PRIMARY KEY,
+    film_id integer,
+    full_name character varying,
+    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (film_id) REFERENCES dim_film(film_id)
+);
+
+CREATE TABLE dim_geography (
+    geography_id integer PRIMARY KEY,
+    country character varying,
+    city character varying,
+    postal_code character varying,
+    address character varying,
+    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE dim_customer (
+    customer_id integer PRIMARY KEY,
+    full_name character varying,
+    active boolean,
+    geography_id integer,
+    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (geography_id) REFERENCES dim_geography(geography_id)
+);
+
 CREATE TABLE dim_staff (
     staff_id integer PRIMARY KEY,
     full_name character varying,
     store_id integer,
     active boolean,
-    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (store_id) REFERENCES dim_store(store_id)
 );
 
 CREATE TABLE dim_store (
     store_id integer PRIMARY KEY,
     manager character varying,
     geography_id integer,
-    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-    FOREIGN KEY (geography_id) REFERENCES Dim_geography(geography_id)
+    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (geography_id) REFERENCES dim_geography(geography_id)
 );
 
 CREATE TABLE dim_tiempo (
@@ -91,25 +101,16 @@ BEGIN
     END LOOP;
 END $$;
 
-CREATE TABLE dim_geography (
-    geography_id integer PRIMARY KEY,
-    country character varying,
-    city character varying,
-    postal_code character varying,
-    address character varying,
-    fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP
-);
-
 CREATE TABLE fact_inventory (
     store_id integer,
     film_id integer,
     rental_date_id integer NOT NULL,
     return_date_id integer,
     fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (rental_date_id) REFERENCES Dim_tiempo(tiempo_id),
-    FOREIGN KEY (return_date_id) REFERENCES Dim_tiempo(tiempo_id),
-    FOREIGN KEY (store_id) REFERENCES Dim_store(store_id),
-    FOREIGN KEY (film_id) REFERENCES Dim_film(film_id)
+    FOREIGN KEY (rental_date_id) REFERENCES dim_tiempo(tiempo_id),
+    FOREIGN KEY (return_date_id) REFERENCES dim_tiempo(tiempo_id),
+    FOREIGN KEY (store_id) REFERENCES dim_store(store_id),
+    FOREIGN KEY (film_id) REFERENCES dim_film(film_id)
 );
 
 CREATE TABLE fact_rental (
@@ -119,9 +120,8 @@ CREATE TABLE fact_rental (
     payment_date_id integer,
     payment_amount numeric(5,2),
     fecha_carga timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (customer_id) REFERENCES Dim_customer(customer_id),
-    FOREIGN KEY (staff_id) REFERENCES Dim_staff(staff_id),
-    FOREIGN KEY (film_id) REFERENCES Dim_film(film_id),
-    FOREIGN KEY (payment_date_id) REFERENCES Dim_tiempo(tiempo_id)
+    FOREIGN KEY (customer_id) REFERENCES dim_customer(customer_id),
+    FOREIGN KEY (staff_id) REFERENCES dim_staff(staff_id),
+    FOREIGN KEY (film_id) REFERENCES dim_film(film_id),
+    FOREIGN KEY (payment_date_id) REFERENCES dim_tiempo(tiempo_id)
 );
-
